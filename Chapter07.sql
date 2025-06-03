a3b4c1231a52f9428f938e6d2e581f7d55db4b22
Grant Fritchey <grant@scarydba.com>
2025-06-02 20:03:29 -0500

    Working on Chapter 7

---
+++ Chapter07.sql
@@ -0,0 +1,116 @@
+--Listing 7-1
+SELECT decp.refcounts,
+       decp.usecounts,
+       decp.size_in_bytes,
+       decp.cacheobjtype,
+       decp.objtype,
+       decp.plan_handle
+FROM sys.dm_exec_cached_plans AS decp;
+
+
+
+--Listing 7-2
+SELECT soh.SalesOrderNumber,
+       soh.OrderDate,
+       sod.OrderQty,
+       sod.LineTotal
+FROM Sales.SalesOrderHeader AS soh
+    JOIN Sales.SalesOrderDetail AS sod
+        ON soh.SalesOrderID = sod.SalesOrderID
+WHERE soh.CustomerID = 29690
+      AND sod.ProductID = 711;
+
+
+
+--Listing 7-3
+SELECT c.usecounts,
+       c.cacheobjtype,
+       c.objtype
+FROM sys.dm_exec_cached_plans AS c
+    CROSS APPLY sys.dm_exec_sql_text(c.plan_handle) AS t
+WHERE t.text = 'SELECT soh.SalesOrderNumber,
+       soh.OrderDate,
+       sod.OrderQty,
+       sod.LineTotal
+FROM Sales.SalesOrderHeader AS soh
+    JOIN Sales.SalesOrderDetail AS sod
+        ON soh.SalesOrderID = sod.SalesOrderID
+WHERE soh.CustomerID = 29690
+      AND sod.ProductID = 711;';
+
+
+--Listing 7-4
+SELECT soh.SalesOrderNumber,
+       soh.OrderDate,
+       sod.OrderQty,
+       sod.LineTotal
+FROM Sales.SalesOrderHeader AS soh
+    JOIN Sales.SalesOrderDetail AS sod
+        ON soh.SalesOrderID = sod.SalesOrderID
+WHERE soh.CustomerID = 29500
+      AND sod.ProductID = 711;
+
+
+
+--Listing 7-5
+SELECT c.usecounts,
+       c.cacheobjtype,
+       c.objtype,
+       t.text,
+       c.plan_handle
+FROM sys.dm_exec_cached_plans AS c
+    CROSS APPLY sys.dm_exec_sql_text(c.plan_handle) AS t
+WHERE t.text LIKE 'SELECT soh.SalesOrderNumber,
+       soh.OrderDate,
+       sod.OrderQty,
+       sod.LineTotal
+FROM Sales.SalesOrderHeader AS soh
+    JOIN Sales.SalesOrderDetail AS sod
+        ON soh.SalesOrderID = sod.SalesOrderID%';
+
+
+
+--Listing 7-6
+EXEC sys.sp_configure 'show advanced option', '1';
+GO
+RECONFIGURE;
+GO
+EXEC sys.sp_configure 'optimize for ad hoc workloads', 1;
+GO
+RECONFIGURE;
+
+
+DBCC FREEPROCCACHE();
+
+
+SELECT c.usecounts,
+       c.cacheobjtype,
+       c.objtype,
+       c.size_in_bytes
+FROM sys.dm_exec_cached_plans AS c
+    CROSS APPLY sys.dm_exec_sql_text(c.plan_handle) AS t
+WHERE t.text = 'SELECT soh.SalesOrderNumber,
+       soh.OrderDate,
+       sod.OrderQty,
+       sod.LineTotal
+FROM Sales.SalesOrderHeader AS soh
+    JOIN Sales.SalesOrderDetail AS sod
+        ON soh.SalesOrderID = sod.SalesOrderID
+WHERE soh.CustomerID = 29690
+      AND sod.ProductID = 711;';
+
+
+
+--Listing 7-7
+EXEC sp_configure 'optimize for ad hoc workloads', 0;
+GO
+RECONFIGURE;
+GO
+EXEC sp_configure 'show advanced option', '0';
+GO
+RECONFIGURE;
+
+
+
+
+
