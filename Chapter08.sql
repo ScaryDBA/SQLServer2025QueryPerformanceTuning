96239a6b58438ac99d7f22603bcf0fbc5d234501
Grant Fritchey <grant@scarydba.com>
2025-06-10 19:42:34 -0500

    Working Chapter 8

--- Chapter08.sql
+++ Chapter08.sql
@@ -1,1 +1,29 @@
 --Listing 8-1
+CREATE OR ALTER PROCEDURE dbo.WorkOrder
+AS
+SELECT wo.WorkOrderID,
+       wo.ProductID,
+       wo.StockedQty
+FROM Production.WorkOrder AS wo
+WHERE wo.StockedQty
+BETWEEN 500 AND 700;
+
+
+EXEC dbo.WorkOrder;
+
+
+--Listing 8-2
+CREATE INDEX IX_Test ON Production.WorkOrder (StockedQty, ProductID);
+
+
+
+
+
+
+
+
+
+
+SELECT dxmv.map_value
+FROM sys.dm_xe_map_values AS dxmv
+WHERE dxmv.name = 'statement_recompile_cause';
