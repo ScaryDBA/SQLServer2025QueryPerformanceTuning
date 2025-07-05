f8ea2cc204cbd3c1233dd37dce20a425e4a638b6
Grant Fritchey <grant@scarydba.com>
2025-07-05 07:34:21 -0500

    Up to Listing 10-3

--- Chapter10.sql
+++ Chapter10.sql
@@ -2,3 +2,25 @@
 SELECT A.PostalCode
 FROM Person.ADDRESS AS A
 WHERE A.StateProvinceID = 42;
+--GO 50
+
+
+--Listing 10-2
+CREATE NONCLUSTERED INDEX IX_Address_StateProvinceID
+ON Person.ADDRESS (StateProvinceID ASC)
+INCLUDE (PostalCode)
+WITH (DROP_EXISTING = ON);
+
+
+--Listing 10-3
+CREATE NONCLUSTERED INDEX IX_Address_StateProvinceID
+ON Person.ADDRESS (StateProvinceID ASC)
+WITH (DROP_EXISTING = ON);
+
+
+
+
+
+
+
+
