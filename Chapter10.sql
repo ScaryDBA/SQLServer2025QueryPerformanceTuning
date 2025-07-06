4956174482a663f307f08ccc99d43c8dc0c968fa
Grant Fritchey <grant@scarydba.com>
2025-07-06 09:07:14 -0500

    Up to listing 10-5, 11 pages of 31.

--- Chapter10.sql
+++ Chapter10.sql
@@ -18,6 +18,33 @@ ON Person.ADDRESS (StateProvinceID ASC)
 WITH (DROP_EXISTING = ON);
 
 
+--Listing 10-4
+SELECT soh.SalesPersonID,
+       soh.OrderDate
+FROM Sales.SalesOrderHeader AS soh
+WHERE soh.SalesPersonID = 276
+      AND soh.OrderDate
+      BETWEEN '4/1/2013' AND '7/1/2013';
+GO 50
+
+
+--Listing 10-3
+CREATE NONCLUSTERED INDEX IX_Test
+ON Sales.SalesOrderHeader (OrderDate ASC);
+
+
+
+--Listing 10-4
+CREATE NONCLUSTERED INDEX IX_Test
+ON Sales.SalesOrderHeader (
+                              SalesPersonID,
+                              OrderDate ASC
+                          )
+WITH DROP_EXISTING;
+
+
+--Listing 10-5
+DROP INDEX IX_Test ON Sales.SalesOrderHeader;
 
 
 
