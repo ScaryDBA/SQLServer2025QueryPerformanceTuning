1377472fa7d89e9fe2dbe72d0a44ceedef5db324
Grant Fritchey <grant@scarydba.com>
2025-07-22 11:18:45 -0500

    Updates through 15-15

--- Chapter15.sql
+++ Chapter15.sql
@@ -95,3 +95,114 @@ FROM Sales.SalesOrderDetail AS sod;
 GO 50
 
 
+--Listing 15-6
+CREATE INDEX TestIndex ON Sales.SalesOrderDetail (UnitPrice ASC);
+
+
+--Listing 15-7
+DROP INDEX IF EXISTS TestIndex ON dbo.Test1;
+
+
+--Listing 15-8
+DECLARE @Id INT = 67260;
+SELECT p.Name,
+       p.ProductNumber,
+       th.ReferenceOrderID
+FROM Production.Product AS p
+    JOIN Production.TransactionHistory AS th
+        ON th.ProductID = p.ProductID
+WHERE th.ReferenceOrderID = @Id;
+--GO 50
+
+--Listing 15-9
+SELECT p.Name,
+       p.ProductNumber,
+       th.ReferenceOrderID
+FROM Production.Product AS p
+    JOIN Production.TransactionHistory AS th
+        ON th.ProductID = p.ProductID
+WHERE th.ReferenceOrderID = 67260;
+--GO 50
+
+
+--Listing 15-10
+SET NOCOUNT ON;
+
+
+--Listing 15-11
+DROP TABLE IF EXISTS dbo.Test1;
+CREATE TABLE dbo.Test1
+(
+    C1 TINYINT
+);
+GO
+DBCC SQLPERF(LOGSPACE);
+--Insert 10000 rows
+DECLARE @Count INT = 1;
+WHILE @Count <= 10000
+BEGIN
+    INSERT INTO dbo.Test1
+    (
+        C1
+    )
+    VALUES
+    (@Count % 256);
+    SET @Count = @Count + 1;
+END;
+DBCC SQLPERF(LOGSPACE);
+
+
+--Listing 15-12
+DECLARE @Count INT = 1;
+DBCC SQLPERF(LOGSPACE);
+BEGIN TRANSACTION;
+WHILE @Count <= 10000
+BEGIN
+    INSERT INTO dbo.Test1
+    (
+        C1
+    )
+    VALUES
+    (@Count % 256);
+    SET @Count = @Count + 1;
+END;
+COMMIT;
+DBCC SQLPERF(LOGSPACE);
+
+--Listing 15-13
+DBCC SQLPERF(LOGSPACE);
+BEGIN TRANSACTION;
+WITH Tally
+AS (SELECT ROW_NUMBER() OVER (ORDER BY (SELECT NULL)) AS num
+    FROM master.dbo.syscolumns AS A
+        CROSS JOIN master.dbo.syscolumns AS B)
+INSERT INTO dbo.Test1
+(
+    C1
+)
+SELECT TOP 1000
+       (Tally.num % 256)
+FROM Tally;
+COMMIT;
+DBCC SQLPERF(LOGSPACE);
+
+
+--Listing 15-14
+SELECT * FROM <TableName> WITH(PAGLOCK);  --Use page level lock
+
+
+--Listing 15-15
+ALTER DATABASE <DatabaseName> SET READ_ONLY;
+
+
+--Listing 15-16
+
+
+
+
+
+
+
+
+
+
